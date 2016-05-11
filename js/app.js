ad12bd781ad0f6c20aa34b1bd2cff3badb68a01e
Edy Segura <edysegura@gmail.com>
2016-05-10 22:11:27 -0300

    Added JavaScript to add names

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
 var names = ['Alison', 'Fabiano', 'Luiz', 'Welligton'];
 
-for (var i = 0; i < names.length; i++) {
-    console.log(names[i]);    
+function addAllNames() {
+    for (var i = 0; i < names.length; i++) {
+        var li = createListItem(names[i]);
+        addNameToList(li);
+    }
 }
+
+function addNameToList(li) {
+  var ul = document.getElementById('list-name');
+  ul.appendChild(li);
+}
+
+function createListItem(name) {
+    var li = document.createElement('li');
+    li.innerHTML = name;
+    return li;
+}
+
+//init
+addAllNames();
