059c00d63587b9afca818c8329fc3e7be8968c1e
Edy Segura <edysegura@gmail.com>
2016-05-31 22:18:52 -0300

    HTML intregation to display added name

--- js/ListController.js
+++ js/ListController.js
@@ -12,10 +12,37 @@ var ListController = {
     handleEventSubmit: function (event) {
         console.log('Event submit was trigged!');
         var inputName = document.getElementById('name');
-        console.log(inputName.value);
+        
+        ListController.addName(inputName.value);
+        
         inputName.value = "";
         //we need this to avoid the form submittion to a server
         event.preventDefault();
+    },
+    
+    addName: function(name) {
+        ListService.addName(name);
+        //TODO move it to HTMLService.js
+        ListController.displayAddedName(name);
+    },
+    
+    //TODO move it to HTMLService.js
+    displayAddedName: function(text) {
+       var li = ListController.createListItem(text);
+       ListController.addToList(li);
+    },
+    
+    //TODO move it to HTMLService.js
+    createListItem: function (text) {
+        var li = document.createElement('li');
+        li.innerHTML = text;
+        return li;
+    },
+    
+    //TODO move it to HTMLService.js
+    addToList: function (listItem) {
+        var ul = document.getElementById('list-name');
+        ul.appendChild(listItem);
     }
     
 };
