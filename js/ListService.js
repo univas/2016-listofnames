7da022023372892a0cd024abea945b66b3537225
Edy Segura <edysegura@gmail.com>
2016-06-06 23:52:30 -0300

    Convert ListService.js to self-executing function

--- js/ListService.js
+++ js/ListService.js
@@ -1,27 +1,36 @@
-var ListService = {
+var ListService = (function () {
 
-    names: [],
+    //private attribute
+    var _names = [];
 
-    getList: function () {
-        ListService.loadNames();
-        return ListService.names;
-    },
+    //private method
+    function _loadNames() {
+        var namesJsonText = window.localStorage.getItem('list-names');
+        if (namesJsonText) {
+            _names = JSON.parse(namesJsonText);
+        }
+    }
 
-    addName: function (name) {
-        ListService.names.push(name);
-        ListService.saveNames();
-    },
-
-    loadNames: function () {
-       var namesJsonText = window.localStorage.getItem('list-names');
-       if(namesJsonText) {
-           ListService.names = JSON.parse(namesJsonText);
-       }
-    },
-
-    saveNames: function () {
-        var namesJsonText = JSON.stringify(ListService.names);
+    function _saveNames() {
+        var namesJsonText = JSON.stringify(_names);
         window.localStorage.setItem('list-names', namesJsonText);
     }
-    
-};
+
+    //public method
+    function getList() {
+        _loadNames();
+        return _names;
+    }
+
+    function addName(name) {
+        _names.push(name);
+        _saveNames();
+    }
+
+    //making public method accessable from outside world
+    return {
+        "getList": getList,
+        "addName": addName
+    };
+
+})();
